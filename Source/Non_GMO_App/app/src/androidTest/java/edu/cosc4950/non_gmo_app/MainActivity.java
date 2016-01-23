8cd8588da333da674f4279f7ccb53543efb32a5e
Michael McCormick <mmccorm4@gmail.com>
2016-01-23 11:32:02 -0700

    Troubles with Manifest
    After lots of trials, I am having some problems with the
    AndroidManifest.xml. For some reason when I want to start a <activity,
    it won’t allow me to link the activity with MainActivity. It has
    something to do with the location of the files. Been trying to figure
    this out all break. Gotten nowhere.

--- Source/Non_GMO_App/app/src/androidTest/java/edu/cosc4950/non_gmo_app/MainActivity.java
+++ Source/Non_GMO_App/app/src/androidTest/java/edu/cosc4950/non_gmo_app/MainActivity.java
@@ -1,18 +1,30 @@
 package edu.cosc4950.non_gmo_app;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.support.v7.widget.Toolbar;
 
 /**
- * Created by Mike on 12/16/15.
+ * Created by Mike on 1/23/16.
  */
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab3);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_numpad);
+            }
+        });
     }
+
 }
