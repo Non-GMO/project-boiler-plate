2e7db63baa19c62c24855d0e3a269d1035a30a0b
Michael McCormick <mmccorm4@gmail.com>
2016-02-10 11:04:02 -0700

    Dropped Vision API
    Stopped working with Vision API and started working with Ward code for
    Camera Preview.

--- Source/Non-GMO/app/src/main/java/non_gmo/cosc4950/edu/non_gmo/MainActivity.java
+++ Source/Non-GMO/app/src/main/java/non_gmo/cosc4950/edu/non_gmo/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -14,9 +15,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    MainFragment myFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +47,13 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        setContentView(R.layout.fragment_cam2);
+        if (savedInstanceState == null) {
+            myFrag = new MainFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, myFrag).commit();
+        }
     }
 
     @Override
