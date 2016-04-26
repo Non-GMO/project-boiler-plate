13412b1a50bdd6fbf3f158ca5eaaceb2fa7ad59d
Michael McCormick <mmccorm4@gmail.com>
2016-04-25 21:46:07 -0600

    Cant get Listview to populate

---
+++ Source/Non-GMO/app/src/main/java/non_gmo/cosc4950/edu/non_gmo/Category.java
@@ -0,0 +1,16 @@
+package non_gmo.cosc4950.edu.non_gmo;
+
+/**
+ * Created by mike on 4/25/16.
+ */
+public class Category {
+
+    private String category;
+
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
